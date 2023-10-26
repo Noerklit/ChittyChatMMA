6a1bca9cad8664024d70ec3a6fdf228f0b703130
Noerklit <mchrnj@gmail.com>
2023-10-26 22:33:27 +0200

    First somewhat working version of ChittyChat

---
+++ client/client.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	proto "chittyChat/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var lamport int64
+
+type Client struct {
+	stream 		proto.ChittyChatService_JoinChatClient
+	Id    		int64
+	clientName  string
+}
+
+func main() {
+	lamport = 0
+
+	const serverID = "localhost:50051"
+
+	// Set up a connection to the server.
+	log.Println("Connecting to: " + serverID)
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC server: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a client
+	client := proto.NewChittyChatServiceClient(conn)
+
+	ch := Client{}
+	ch.clientConfig()
+
+	rand.Seed(time.Now().UnixNano())
+	ch.Id = rand.Int63n(1000000)
+
+	lamport++
+	var user = proto.User{
+		Id:	  ch.Id,
+		Name: ch.clientName,
+		Lamport: lamport,
+	}
+
+	// Create a stream context
+	_stream, err := client.JoinChat(context.Background(), &user)
+	if err != nil {
+		log.Fatalf("Failed to get a response from the server: %v", err)
+	}
+
+	ch.stream = _stream
+
+	SetupCloseHandler(ch, client)
+
+	go ch.sendMessage(client)
+	go ch.receiveMessage()
+
+	//Block to block main
+	bl := make(chan bool)
+	<-bl
+}
+
+//Function to assign a name to the client
+func (ch *Client) clientConfig() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your name: ")
+	msg, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatalf("Failed to read client name from console: %v", err)
+	}
+	//Remove the carriage return \r and newline characters \n
+	ch.clientName = strings.TrimRight(msg, "\r\n") 
+}
+
+func (ch *Client) sendMessage(client proto.ChittyChatServiceClient) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		clientMessage, err := reader.ReadString('\n')
+		clientMessage = strings.TrimRight(clientMessage, "\r\n")
+		if err != nil {
+			log.Fatalf("Failed to read client message from console: %v", err)
+			continue
+		}
+		if len(clientMessage) > 0 && len(clientMessage) <= 128 {
+			lamport++
+			msg := proto.FromClient {
+				Name: ch.clientName,
+				Content: clientMessage,
+				Lamport: lamport,
+			}
+			_, err := client.PublishMessage(context.Background(), &msg)
+			if err != nil {
+				log.Printf("Error whilst sending message to server: %v", err)
+			}
+
+			time.Sleep(450 * time.Millisecond)
+		} else {
+			log.Printf("Only messages with a length between 1 and 128 characters are valid")
+		}
+	}
+}
+
+func (ch *Client) receiveMessage() {
+	for {
+		resp, err := ch.stream.Recv()
+		if err != nil {
+			log.Printf("Could not receive: %v", err)
+		}
+		
+		incomingLamport := resp.Lamport
+		lamport = max(lamport, incomingLamport)
+		lamport++
+
+		log.Printf("%s: %s", resp.Name, resp.Content)
+	}
+}
+
+func SetupCloseHandler(ch Client, client proto.ChittyChatServiceClient) {
+	cl := make(chan os.Signal)
+	signal.Notify(cl, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-cl
+		fmt.Println("\nClosing connection to server...")
+		lamport++
+		client.LeaveChat(context.Background(), &proto.User{Id: ch.Id, Name: ch.clientName, Lamport: lamport})
+		os.Exit(0)
+	}()
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	proto "chittyChat/grpc"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+var lamport int64
+
+type ChittyChatServer struct {
+	proto.UnimplementedChittyChatServiceServer
+}
+
+var users = make(map[int64]proto.ChittyChatService_JoinChatServer) // map of users
+
+func main() {
+	lamport = 0
+
+	//Start the gRPC server
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 50051: %v", err)
+	}
+	log.Printf("Starting ChittyChat server on port 50051...")
+
+	//Create an empty gRPC server
+	gRPCServer := grpc.NewServer()
+
+	//Create a new instance of the ChittyChatServer and bind it to our empty gRPC server
+	ccs := ChittyChatServer{}
+	proto.RegisterChittyChatServiceServer(gRPCServer, &ccs)
+
+	err = gRPCServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("Failed to serve gRPC server on port 50051: %v", err)
+	}
+}
+
+func (j *ChittyChatServer) JoinChat(user *proto.User, ccsi proto.ChittyChatService_JoinChatServer) error {
+	incomingLamport := user.Lamport
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
+	users[user.Id] = ccsi
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("Oh god press the panic button: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	content := fmt.Sprintf("%s joined the chat", user.Name)
+
+	Broadcast(&proto.FromClient{Name: "ServerMessage", Content: content, Lamport: lamport})
+
+	//Function to block
+	bl := make(chan bool)
+	<-bl
+
+	return nil
+}
+
+func (l *ChittyChatServer) LeaveChat(ctx context.Context, user *proto.User) (*proto.Empty, error) {
+	incomingLamport := user.Lamport
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
+	//Remove the participant 
+	delete(users, user.Id)
+
+	//Update the timestamp
+	Broadcast(&proto.FromClient{Name: "ServerMessage", Content: fmt.Sprintf("%s left the chat", user.Name), Lamport: lamport})
+	return &proto.Empty{}, nil
+}
+
+func (p *ChittyChatServer) PublishMessage(ctx context.Context, msg *proto.FromClient) (*proto.Empty, error) {
+	incomingLamport := msg.Lamport
+	lamport = max(lamport, incomingLamport)
+	lamport++
+
+	msg.Lamport = lamport
+
+	Broadcast(msg)
+	return &proto.Empty{}, nil
+}
+
+func Broadcast(msg *proto.FromClient) {
+	name := msg.Name
+	content := msg.Content
+	lamport := msg.Lamport
+
+	log.Printf("%s, %s, %d", name, content, lamport)
+
+	for key, value := range users {
+		err := value.Send(&proto.FromServer{Name: name, Content: content, Lamport: lamport})
+		if err != nil {
+			log.Printf("Failed to broadcast message to" + string(key)+ ": ", err)
+			log.Printf("Failed to broadcast message to %s: %v", fmt.Sprint(key), err)
+		}
+	}
+}
+
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
